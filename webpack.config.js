8894c63d72685ba586443a8b1e40bd099b3de559
이경준 <gyoungjun_lee@tmax.co.kr>
2020-12-15 17:02:01 +0900

    [feat] css, file loader 추가

--- webpack.config.js
+++ webpack.config.js
@@ -27,6 +27,21 @@ module.exports = {
           },
         ],
       },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      // npm i -D file-loader
+      {
+        test: /\.png$/,
+        use: [{
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]?[hash]' // 이미지 이름을 동적으로 설정해주도록
+            //publicPath: '../dist' //이미지를 가져올 때는 dist 폴더에 있는 거를 가져와야함. (이거는 htmlwebpackplugin 설정 이전에 해준거고 설정 이후에는 자동으로 해줘서 필요 없음)
+          }
+        }]
+      },
     ],
   },
   plugins: [
